651e73c4a84b9e1ac0560538e4ffce6242033947
fr1t2 <fr1t2@protonmail.com>
2021-03-20 08:02:52 -0600

    remove extra punctuation

--- _scripts/discord/cmd/add.js
+++ _scripts/discord/cmd/add.js
@@ -148,7 +148,7 @@ module.exports = {
             ReplyMessage('Be right back, generating a new quantum secure address for you to send tips from...');
             // Create user wallet
             const addedUser = dbHelper.addedUser;
-            const addedUserPromise = addedUser({ service_id: `@${MessageAuthorID}`, service: 'discord' });
+            const addedUserPromise = addedUser({ service_id: `'@${MessageAuthorID}'`, service: 'discord' });
             addedUserPromise().then(function(){
 
               const qrlWal = wallet.CreateQRLWallet;
