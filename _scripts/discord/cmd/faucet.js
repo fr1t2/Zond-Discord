d9a275e7714868dca73ca7882fde08fd90ab5cd4
fr1t2 <fr1t2@protonmail.com>
2022-01-22 11:58:49 -0700

    fix: logic for fixed payout

--- _scripts/discord/cmd/faucet.js
+++ _scripts/discord/cmd/faucet.js
@@ -316,7 +316,7 @@ module.exports = {
               const user_id = userInfoArray[0][0].user_id;
               const fixedPayout = new Boolean(config.faucet.fixed_payout);
               let Drip
-              if (fixedPayout) {
+              if (fixedPayout === true) {
                 Drip = config.faucet.fixed_amount;
               }
               else {
