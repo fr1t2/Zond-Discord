d756b3f8b75fa0f12c54d9432686a8c8df48ac48
fr1t2 <fr1t2@protonmail.com>
2022-09-09 12:48:09 -0600

    fix: Messaging output

--- _scripts/discord/cmd/one.js
+++ _scripts/discord/cmd/one.js
@@ -16,23 +16,14 @@ module.exports = {
     const config = require('../../../_config/config.json');
     const emojiCharacters = require('../../emojiCharacters');
     const uuid = `${message.author}`;
-
     const service_id = uuid.slice(1, -1);
-
     const GetAllUserInfo = dbHelper.GetAllUserInfo;
     const checkUserPlusOne = plusOneHelper.CheckPlusOne;
     const insertUserPlusOne = plusOneHelper.InsertPlusOne;
     const updateUserPlusOne = plusOneHelper.UpdatePlusOne;
     const updateUserPlusOneKey = plusOneHelper.UpdatePlusOneKey
-
     const getBalance = wallet.GetBalance;
-
-
     const userInfoArray = [];
-//    const faucetResponse = require('../faucet-response.json');
-//    const totalDrips = faucetHelper.totalPaid;
-//    const faucetDrip = faucetHelper.Drip;
-
 
     // ReplyMessage(' Check your DM\'s');
     function ReplyMessage(content) {
@@ -95,64 +86,11 @@ module.exports = {
       }, 1000);
     }
 
-
-//    function millisToMinutesAndSeconds(millisec) {
-//      let seconds = (millisec / 1000).toFixed(0);
-//      let minutes = Math.floor(seconds / 60);
-//      let hours = '';
-//      if (minutes > 59) {
-//        hours = Math.floor(minutes / 60);
-//        hours = (hours >= 10) ? hours : '0' + hours;
-//        minutes = minutes - (hours * 60);
-//        minutes = (minutes >= 10) ? minutes : '0' + minutes;
-//      }
-//      seconds = Math.floor(seconds % 60);
-//      seconds = (seconds >= 10) ? seconds : '0' + seconds;
-//      if (hours != '') {
-//        if ( seconds == 0) {
-//          if (minutes == 0) {
-//            return hours + 'hr';  
-//          }
-//          return hours + 'hr ' + minutes + 'min';  
-//        }
-//        return hours + 'hr ' + minutes + 'min ' + seconds + 'sec';
-//      }
-//      if (seconds == 0 ) {
-//        return minutes + 'min';
-//      }
-//      else if (minutes == 0 ) {
-//        return seconds + 'sec';
-//      }
-//      else {
-//        return minutes + 'min ' + seconds + 'sec';
-//      }
-//    }
-
-
-
     // check if this is a DM and if so, block forcing user into the chat room
     if (message.channel.type === 'dm' && args[0] != "verify") {
       errorMessage({ error: 'Can\'t access +one from DM!', description: 'Please try again from the [QRL Discord Server](https://theqrl.org/discord), this function will only work there.' });
       return;
     }
-
-      // If not specified channel, fail and remove bots message
-//    if (message.channel.name !== 'bot') {
-      // check if the message is in an approved channel
-      // TODO: Add this to the config and parse from a list of approved channels
-//      message.channel.startTyping();
-//      setTimeout(function() {
-//        message.channel.stopTyping(true);
-//        message.reply('Please use the #bot channel for faucet functions...')
-//          // delete the message after a bit
-//          .then(msg => {
-//            setTimeout(() => msg.delete(), 10000)
-//          })
-//          .catch( );
-//      }, 500);
-//      return;
-//    }
-//
 
     // check for a balance in the +one wallet first
     async function oneWalletBalance() {
@@ -164,8 +102,6 @@ module.exports = {
         });
       });
     }
-
-
 
     async function checkUser(user) {
       return new Promise(resolve => {
@@ -227,6 +163,7 @@ module.exports = {
       });
     }
 
+/*
     async function insertPlusOne(user_id) {
       // Insert user into plusone table
       return new Promise(resolve => {
@@ -239,18 +176,6 @@ module.exports = {
       });
     }
 
-//    async function updatePlusOne(user_id) {
-//      // Update plusone table when tx is paid out
-//      return new Promise(resolve => {
-//        const check_info = { service: 'discord', user_id: user_id };
-//        const checkFaucetPromise = updateUserPlusOne(check_info);
-//        // fail from the start
-//        checkFaucetPromise.then(function(results) {
-//          resolve(results);
-//        });
-//      });
-//    }
-
     async function updatePlusOneKey(user_id) {
       // Update plusone key when user requests
       return new Promise(resolve => {
@@ -262,25 +187,30 @@ module.exports = {
         });
       });
     }
+*/
 
-// --------------------------------------------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------------------------- //
 // Main Program
-// --------------------------------------------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------------------------- //
 /*
 
 - Check for balance in plusone wallet address
 - Check for user in tipbot, if not found prompt to sign up
 - Check for user in plusone table already
+  
+  // future implementation //
   - If found and user didn't enter args, send message showing user already completed
   - IF args key, generate a new key (hash) and send the salt to the user in DM
   - IF args verify AND user is authorized AND in DM, using list of salts provided to verify they exist. 
     - Results returned in csv/json including discord user ID, time signed up or nothing if not found 
+    // ////////////////// //
+
   - IF NOT found add user to plusone table and return verification message
 
 Payout happens in a separate script combining a group up to 100 addresses together ran through cron.
 
 */
-// --------------------------------------------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------------------------- //
     const userArray = [];
     oneWalletBalance()
     // Check wallet and return if no funds there
@@ -293,25 +223,16 @@ Payout happens in a separate script combining a group up to 100 addresses togeth
         }
         // Check for the user and if not signed up, opted out or not agreed fail and error
         checkUser(service_id).then(function(userCheckResponse) {
-        
-
-          console.log(JSON.stringify(userCheckResponse));
           userArray.push(userCheckResponse);
-          console.log(JSON.stringify(userArray))
-
           // logic to check if user is found before proceeding
           if (!userArray[0][1].checkUserPassed) {
             return
           }
-          console.log('userID:  ' + userArray[0][0][0].user_id)
           checkPlusOne(userArray[0][0][0].user_id).then(function(plusOneCheck) {
-            
-            console.log(JSON.stringify(plusOneCheck));
-            
-
+/*
+  Not implemented yet, though placeholder in database for the function is there.
 
             if ( args[0] == "verify") {
-
               console.log("uuid:\t" + uuid.slice(2, -1) + "\nplusone_admin:\t" + config.plusone.plusone_admin + "\nbot admin\t" + config.discord.bot_admin)
               // is user authorized
               if (uuid.slice(2, -1) == config.plusone.plusone_admin || uuid.slice(2, -1) == config.discord.bot_admin ) {
@@ -329,25 +250,25 @@ Payout happens in a separate script combining a group up to 100 addresses togeth
                 return;
               }
       
-              // send message to request list and wait fro the list to be sent (csv)
-
+              // send message to request list and wait for the list to be sent? (csv)
               // Using this list, hash the salt and see if it matches something in the DB, 
-
               // if so return the users data in a csv
 //              return;
             }
+*/
             if (plusOneCheck.plusone_found === "true") {
               // User is found in the table and has been paid previously
 
+/*
               if (args[0] == "key" || args[0] == "Key" || args[0] == "onekey") {
                 // regenerate a key and send the details
-                oneErrorMessage({ error: 'Not Implimented', description: 'This function is not implemented yet...' });
+                errorMessage({ error: 'Not Implemented', description: 'This function is not implemented yet...' });
 
                 return;
               }
+*/
 
               oneErrorMessage({ error: 'User Signed Up Previously', description: '<@' + message.author + '>, you have previously signed up, one time per user!\nMore information can be found here: [oneqrl.otd.to](https://oneqrl.otd.to/community/)' });
-
               if (plusOneCheck.paid === 0) {
                 const embed = new Discord.MessageEmbed()
                   .setColor(0x000000)
@@ -362,7 +283,6 @@ Payout happens in a separate script combining a group up to 100 addresses togeth
                     errorMessage({ error: 'Direct Message Disabled...', description: 'It seems you have DM\'s blocked, please enable and try again...' });
                     if (error) return error;
                   });
-
               }
               else if (plusOneCheck.paid === 1) {
                 const embed = new Discord.MessageEmbed()
@@ -381,12 +301,9 @@ Payout happens in a separate script combining a group up to 100 addresses togeth
                   });
               }
             }
-
             else {
               // User not found in table, add them and pay out
-
-              plusOneHelper.InsertPlusOne({service: "discord", user_id: userArray[0][0][0].user_id, one_key: '', one_amt: 1 })
-
+              plusOneHelper.InsertPlusOne({service: "discord", user_id: userArray[0][0][0].user_id, one_key: '', one_amt: conf.plusone.fixed_amount });
               //oneMessage('One QRL, One Community!!\nYou have signed up, One QRL is on the way!');
               oneMessage({ source: 'https://oneqrl.otd.to/community/', title: 'OneQRL', message: 'Little things we do, make us what we are. 1 QRL is on the way to your tipbot account. Thanks for signing up!' });
               message.react(emojiCharacters.o)
@@ -397,10 +314,8 @@ Payout happens in a separate script combining a group up to 100 addresses togeth
                 .then(() => message.react(emojiCharacters.l))
               .catch(() => console.error('One of the emojis failed to react.'));
             }
-
           });
         });
       });
-
   },
 };
